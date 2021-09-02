340dbba949cf8fc90fad41674868a7f6c3fe6d7f
natalieky96 <natalie203398>
2021-09-02 18:10:47 +0300

    I added register activity_main.xml

---
+++ app/src/main/java/com/brave/registration/regist/app/Register_admin.java
@@ -0,0 +1,14 @@
+package com.brave.registration.regist.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_admin);
+    }
+}
